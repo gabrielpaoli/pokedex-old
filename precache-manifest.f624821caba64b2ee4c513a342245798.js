3344d880c1caa28637763e3517b67365cf16e191
gabrielpaoli <gabrielpaoli@hotmail.com>
2020-08-04 12:40:03 -0300

    Updates

---
+++ precache-manifest.f624821caba64b2ee4c513a342245798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad3df5ffc2249465dbbbd82ca696ae6",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "90616f73cf153091a9cc",
+    "url": "/pokedex/static/js/2.3f1a53aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.3f1a53aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225833f1fca65f91d701",
+    "url": "/pokedex/static/js/main.a5ab3694.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
